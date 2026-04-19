31a8f3cfd5774ccfbe76798a94f78321993741c2
badexx <iwoplo111@gmail.com>
2026-04-19 21:44:53 +0200

    more songs added

--- songs.js
+++ songs.js
@@ -3,5 +3,14 @@ const songs = [
     {src: "songs/Słup.mp3", title: "Słup", artist: "Białas"},
     {src: "songs/Polsilver.mp3", title: "Polsilver", artist: "PRO8L3M"},
     {src: "songs/Symetryczno-liryczna.mp3", title: "Symetryczno-liryczna", artist: "Enej"},
+    {src: "songs/Lili.mp3", title: "Lili", artist: "Enej"},
     {src: "songs/chłopaki jak my.mp3", title: "chłopaki jak my", artist: "Kuban,Favst,Kizo"},
+    {src: "songs/Może Będzie lepiej.mp3", title: "Może Będzie lepiej", artist: "Enej"},
+    {src: "songs/Skrzydlate ręce.mp3", title: "Skrzydlate ręce", artist: "Kuban,Favst,Kizo"},
+    {src: "songs/Nie Chcę Spać.mp3", title: "Nie Chcę Spać", artist: "Enej"},
+    {src: "songs/Kamień z Napisem LOVE.mp3", title: "Kamień z Napisem LOVE", artist: "Enej"},
+    {src: "songs/Zbudujemy dom.mp3", title: "Zbudujemy dom", artist: "Enej"},
+    {src: "songs/Brokeboy.mp3", title: "Brokeboy", artist: "Białas,Szpaku,Tomb"},
+    {src: "songs/Uzi.mp3", title: "Uzi", artist: "Białas,Sobel,Deemz"},
+    {src: "songs/Na Serio.mp3", title: "Na Serio", artist: "Białas,Lanek,Bedoes 2115"}
 ];
